4b453c8b4cda7cbaed038bbe3909faf0536d7161
zSunTz <71081809+zSunTz@users.noreply.github.com>
2020-12-24 23:09:39 +0700

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,28 @@
+function formSubmit(options) {
+    var formElements = document.querySelectorAll(options.formSelector);
+
+    Array.from(formElements).forEach(function(formElement) {
+        var inputElement = formElement.querySelector(options.inputSelector);
+        var submitBtn = formElement.querySelector(options.submitBtnSelector);
+        
+        inputElement.onblur = function() {
+            if (inputElement.value !== "") {
+                submitBtn.classList.add(options.activeBtnClass);
+            } else {
+                submitBtn.classList.remove(options.activeBtnClass);
+            }
+
+            if (!submitBtn.classList.contains(options.activeBtnClass)) {
+                submitBtn.disabled = true;
+            } else {
+                submitBtn.disabled = false;
+            }
+        }
+
+        formElement.onsubmit = function(e) {
+            e.preventDefault();
+            alert("This is just a static web. Thank you for testing <3");
+                
+        }
+    });
+}
